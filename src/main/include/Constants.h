546fbbbc989f6d147cc61401bcb4b3c3ab426cc4
Drew Williams <williams.r.drew@gmail.com>
2019-10-01 14:18:17 -0400

    added swerve turning motor config

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -10,5 +10,14 @@ namespace tigertronics {
 
         //ENCODER INFO
         static constexpr int ctreEncoderTicksPerRev = 4096;
+
+        //SWERVE TURNING MOTOR CONFIG
+        static constexpr double swerveTurningkP = 12;
+        static constexpr double swerveTurningkI = 0;
+        static constexpr double swerveTurningkD = 120;
+        static constexpr units::radians_per_second_t swerveTurningMaxVel = 3600_rad_per_s;
+        //im too lazy to define radial acceleration right now so this is in ticks/100ms/s
+        static constexpr double swerveTurningMaxAccel = 1500;
+        static constexpr int swerveTurningErrorAllowance = 0;
     }
 }
--- src/main/include/tigertronics/SwerveModule.h
+++ src/main/include/tigertronics/SwerveModule.h
@@ -15,7 +15,7 @@
 
 class SwerveModule {
 public:
-    SwerveModule(int driveMotorChannel, int turningMotorChannel);
+    SwerveModule(int driveMotorChannel, int turningMotorChannel, int calibrationValue);
     frc_new::SwerveModuleState GetState();
     void SetDesiredState(const frc_new::SwerveModuleState& state);
 
@@ -24,11 +24,19 @@ private:
     void SetupTurningMotor();
     units::meters_per_second_t ConvertAngularToLinearVelocity(units::radians_per_second_t rpm, units::meter_t radius);
     units::radians_per_second_t ConvertLinearToAngularVelocity(units::meters_per_second_t velocity, units::meter_t radius);
+    units::radians_per_second_t ConvertTalonVelocityToRadiansPerSecond(int ticksPer100ms);
+    int ConvertRadiansPerSecondToTalonVelocity(units::radians_per_second_t radPerSec);
     units::radian_t ConvertEncoderUnitsToRadians(int encoderTicks);
     int ConvertRadiansToEncoderTicks(units::radian_t rads);
     static constexpr units::meter_t kWheelRadius = tigertronics::constants::driveWheelRadius;
     static constexpr int kEncoderResolution = tigertronics::constants::ctreEncoderTicksPerRev;
-
+    static constexpr double kTurnP = tigertronics::constants::swerveTurningkP;
+    static constexpr double kTurnI = tigertronics::constants::swerveTurningkI;
+    static constexpr double kTurnD = tigertronics::constants::swerveTurningkD;
+    static constexpr units::radians_per_second_t kMaxTurnVel = tigertronics::constants::swerveTurningMaxVel;
+    static constexpr double kMaxTurnAccel = tigertronics::constants::swerveTurningMaxAccel;
+    static constexpr int kTurnErrorAllowance = tigertronics::constants::swerveTurningErrorAllowance;
+    int kCalibrationValue;
     rev::CANSparkMax m_driveMotor;
     rev::CANPIDController m_drivePIDController;
     rev::CANEncoder m_driveEncoder;
