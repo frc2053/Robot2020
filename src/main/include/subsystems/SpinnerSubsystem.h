f2fdab8b17a18a3354f29aa70744f281a712f6c6
catherineDeskur <chdeskur@gmail.com>
2020-01-10 17:28:13 -0500

    spinner control

--- src/main/include/subsystems/SpinnerSubsystem.h
+++ src/main/include/subsystems/SpinnerSubsystem.h
@@ -4,15 +4,21 @@
 
 class Spinner : public frc2::SpinnerSubsystem {
     private: 
-    std::unique_ptr<ctre::phoenix::motorcontrol::can::TalonSRX> spinnerMotor;
+    static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
+    rev::ColorSensorV3 m_colorSensor{i2cPort};
+    rev::ColorMatch m_colorMatcher;
     frc::Color color;
-
+    std::unique_ptr<ctre::phoenix::motorcontrol::can::TalonSRX> spinnerMotor;
+    
     public:
     Spinner();
     //checks for fms data
     void Periodic() override;
-    //method to return color
-    frc::Color getColor();
     //method to run motor
     void RunSpinnerMotor(double Speed);
+
+    static constexpr frc::Color kBlueTarget = frc::Color(0.143, 0.427, 0.429);
+    static constexpr frc::Color kGreenTarget = frc::Color(0.197, 0.561, 0.240);
+    static constexpr frc::Color kRedTarget = frc::Color(0.561, 0.232, 0.114);
+    static constexpr frc::Color kYellowTarget = frc::Color(0.361, 0.524, 0.113);
 }
