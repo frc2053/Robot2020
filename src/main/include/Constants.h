4d9768c17700951e6015309e4b2c1b37aa999dd4
Drew Williams <williams.r.drew@gmail.com>
2020-02-20 21:48:59 -0500

    changed wheel diam and can id

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -9,7 +9,7 @@ namespace tigertronics {
         //PHYSICAL DIMENSIONS
         static constexpr units::meter_t drivebaseLength = 24_in;
         static constexpr units::meter_t drivebaseWidth = 23_in;
-        static constexpr units::meter_t driveWheelRadius = 2.25_in / 2;
+        static constexpr units::meter_t driveWheelRadius = 3_in / 2;
         static constexpr double swerveDriveGearReduction = 5.33 / 1;
 
         //COLOR INFO
@@ -112,6 +112,6 @@ namespace tigertronics {
         static constexpr int intakeSolenoidPortRev = 1;
 
         //CONTROL PANEL
-        static constexpr int controlPanel = 10;
+        static constexpr int controlPanel = 19;
     }
 }
