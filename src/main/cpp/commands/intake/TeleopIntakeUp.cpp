baf39ba88c15a4407363793446e39f4aaac68fad
Drew Williams <williams.r.drew@gmail.com>
2020-02-24 13:50:42 -0500

    added some commands

---
+++ src/main/cpp/commands/intake/TeleopIntakeUp.cpp
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/intake/TeleopIntakeUp.h"
+#include "commands/intake/IntakeUp.h"
+#include "commands/intake/SetIntakeSpeed.h"
+#include "commands/intake/SetConveyorSpeed.h"
+#include <frc2/command/WaitUntilCommand.h>
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.
+// For more information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+TeleopIntakeUp::TeleopIntakeUp(IntakeSubsystem* intakeSub) : m_intakeSubsystem(intakeSub) {
+  AddCommands(
+    IntakeUp(intakeSub),
+    SetIntakeSpeed(intakeSub, 0),
+    SetConveyorSpeed(intakeSub, 0)
+  );
+}
